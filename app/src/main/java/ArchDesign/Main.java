09f09cac4b2b02005dcb86494ffea49990d3670b
Kanan Ahmadov <a.kanan@wustl.edu>
2025-10-30 17:30:16 -0500

    Main, Response, Packing

--- app/src/main/java/ArchDesign/Main.java
+++ app/src/main/java/ArchDesign/Main.java
@@ -1,4 +1,5 @@
 package ArchDesign;
+
 import java.util.List;
 
 import ArchDesign.entities.Art;
@@ -7,49 +8,46 @@ import ArchDesign.entities.Client;
 import ArchDesign.parser.Parser;
 import ArchDesign.requests.Request;
 import ArchDesign.responses.JSONSerializer;
+import ArchDesign.responses.CommandLineSerializer;
 import ArchDesign.responses.Response;
 
 public class Main {
+
+    private static final String USAGE = String.join(System.lineSeparator(),
+            "Usage:",
+            "  java -jar app.jar <art_input.csv> <client_input.csv> [output.json]",
+            "",
+            "Examples:",
+            "  java -jar app.jar Input3.csv Client.csv",
+            "  java -jar app.jar Input3.csv Client.csv Output3.json");
+
     public static void main(String[] args) {
-        
-        // TODO: If CLI Cases:
-        // IF CLI < 2: Give Error, too little Args
-        // If CLI = 2: Run Normal with Output in Terminal
-        // If CLI = 3: Run Normal with Output in Terminal as well as Output to a JSON file
-        // If CLI > 3: Give Error, too many Args
 
-        if (args.length < 2 || args.length > 3){
-            // TODO: Good Usage Message
-            System.out.println("Give Usage Message:");
-        }
-        else{
-            String inputFileName = args[0]; // read in from args
-            String inputClientFileName = args[1]; // read in from args
-
-            System.out.println("Input File Name: " + inputFileName);
-            System.out.println("Client File Name: " + inputClientFileName);
-
-            Response response = generateResponseForMain(inputFileName, inputClientFileName);
-            if (args.length == 3){
-                String outputFileName = args[2];
-                new JSONSerializer(response, outputFileName);
-            }
-            System.out.println(response.toString());
+        if (args.length < 2 || args.length > 3) {
+            System.err.println(USAGE);
+            return;
         }
 
+        String inputFileName = args[0];
+        String inputClientFileName = args[1];
+
+        Response response = generateResponseForMain(inputFileName, inputClientFileName);
+
+        if (args.length == 3) {
+            String outputFileName = args[2];
+            new JSONSerializer(response, outputFileName);
+        }
+
+        // CLI summary is produced by the serializer (not the interactor)
+        CommandLineSerializer cli = new CommandLineSerializer(response);
+        System.out.println(cli.getSummary());
     }
 
-    public static Response generateResponseForMain(String inputfile, String clientFile){
+    public static Response generateResponseForMain(String inputfile, String clientFile) {
         List<Art> arts = Parser.parseArt(inputfile);
-
         Client client = Parser.parseClient(clientFile);
-
         Request request = new Request(arts, client);
-
         Packing packing = new Packing();
-
-        Response response = packing.packEverything(request);
-
-        return response;
+        return packing.packEverything(request);
     }
 }
